632bf15f54602837e1f6685fdd67d731d2924a79
/echo <lars.pettersen91@gmail.com>
2025-12-18 12:44:41 +0100

    added beforeEach to login test to keep DRY. added logout test because I wanted to try it

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,8 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("login", () => {
-  test("The user can log in", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("/login");
+  });
+
+  test("The user can log in", async ({ page }) => {
     await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
     await page
       .locator('input[name="password"]')
@@ -14,7 +17,6 @@ test.describe("login", () => {
   });
 
   test("Wrong password shows error", async ({ page }) => {
-    await page.goto("/login");
     await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
     await page.locator('input[name="password"]').fill("ohboyilikeerrors");
     await page.getByRole("button", { name: "Login" }).click();
@@ -22,5 +24,24 @@ test.describe("login", () => {
     await expect(page.locator("#message-container")).toContainText(
       "Invalid email or password",
     );
+  });
+});
+
+test.describe("logout", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/login");
+    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
+    await page
+      .locator('input[name="password"]')
+      .fill(process.env.TEST_USER_PASSWORD);
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+  });
+
+  test("The user can log out", async ({ page }) => {
+    await page.getByRole("button", { name: "Logout" }).click();
+
+    await expect(page.getByRole("button", { name: "Login" })).toBeVisible();
   });
 });
